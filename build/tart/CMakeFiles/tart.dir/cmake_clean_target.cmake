9377f9803ace0847217177cc9bc8ceea34eaec76
me <softcookiepp@protonmail.com>
2025-10-07 23:10:21 -0700

    removed garbage

--- build/tart/CMakeFiles/tart.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtart.a"
-)
--- build/tart/subprocess/src/cpp/CMakeFiles/subprocess.dir/subprocess/PipeVar.cpp.o.d
+++
@@ -1,3 +0,0 @@
-tart/subprocess/src/cpp/CMakeFiles/subprocess.dir/subprocess/PipeVar.cpp.o: \
- /home/me/development/vulkan-blas-shaders/tart/subprocess/src/cpp/subprocess/PipeVar.cpp \
- /usr/include/stdc-predef.h
