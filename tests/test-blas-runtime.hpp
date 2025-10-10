b9ac1c612381945784835515c2fa6b43d61f75e1
softcookiepp <softcookiepp@protonmail.com>
2025-10-10 16:43:39 -0700

    ugh

--- tests/test-blas-runtime.hpp
+++ tests/test-blas-runtime.hpp
@@ -264,6 +264,98 @@ void sscal(tart::command_sequence_ptr sequence, uint32_t n,
 
 // LEVEL 2
 #if 0
+
+void doMatVec(const CBLAS_ORDER layout, const CBLAS_TRANSPOSE a_transpose, const size_t m, const size_t n, const T alpha,
+                      const Buffer<T>& a_buffer, const size_t a_offset, const size_t a_ld, const Buffer<T>& x_buffer,
+                      const size_t x_offset, const size_t x_inc, const T beta, const Buffer<T>& y_buffer,
+                      const size_t y_offset, const size_t y_inc, bool fast_kernel = true, bool fast_kernel_rot = true,
+                      const size_t parameter = 0, const bool packed = false, const size_t kl = 0, const size_t ku = 0)
+{
+	// Makes sure all dimensions are larger than zero
+	if (m == 0 || n == 0) throw std::invalid_argument("dimensions must not be 0");
+
+	// Computes whether or not the matrix has an alternative layout (row or column-major).
+	const auto a_altlayout = (layout == Layout::kRowMajor);
+	auto a_one = (a_altlayout) ? n : m;
+	const auto a_two = (a_altlayout) ? m : n;
+
+	// Swap m and n if the matrix is transposed
+	const auto a_transposed = (a_transpose != CblasNoTrans);
+	const auto m_real = (a_transposed) ? n : m;
+	const auto n_real = (a_transposed) ? m : n;
+
+	// Special adjustments for banded matrices
+	if (kl != 0 || ku != 0) {
+	a_one = kl + ku + 1;
+	}
+
+	// Determines whether the kernel needs to perform rotated access ('^' is the XOR operator)
+	const auto a_rotated = a_transposed ^ a_altlayout;
+
+	// In case of complex data-types, the transpose can also become a conjugate transpose
+	const auto a_conjugate = (a_transpose == Transpose::kConjugate);
+
+	// Tests the matrix and the vectors for validity
+	if (packed) {
+	TestMatrixAP(n, a_buffer, a_offset);
+	} else {
+	TestMatrixA(a_one, a_two, a_buffer, a_offset, a_ld);
+	}
+	TestVectorX(n_real, x_buffer, x_offset, x_inc);
+	TestVectorY(m_real, y_buffer, y_offset, y_inc);
+
+	// Determines whether or not the fast-version can be used
+	fast_kernel = fast_kernel && (a_offset == 0) && (a_rotated == 0) && (a_conjugate == 0) &&
+				IsMultiple(m, db_["WGS2"] * db_["WPT2"]) && IsMultiple(n, db_["WGS2"]) && IsMultiple(a_ld, db_["VW2"]);
+	fast_kernel_rot = fast_kernel_rot && (a_offset == 0) && (a_rotated == 1) && (a_conjugate == 0) &&
+					IsMultiple(m, db_["WGS3"] * db_["WPT3"]) && IsMultiple(n, db_["WGS3"]) &&
+					IsMultiple(a_ld, db_["VW3"]);
+
+	// If possible, run the fast-version (rotated or non-rotated) of the kernel
+	auto kernel_name = std::string{"Xgemv"};
+	const auto m_ceiled = Ceil(m_real, db_["WGS1"] * db_["WPT1"]);
+	auto global_size = m_ceiled / db_["WPT1"];
+	auto local_size = db_["WGS1"];
+	if (fast_kernel) {
+	kernel_name = "XgemvFast";
+	global_size = m_real / db_["WPT2"];
+	local_size = db_["WGS2"];
+	}
+	if (fast_kernel_rot) {
+	kernel_name = "XgemvFastRot";
+	global_size = m_real;
+	local_size = db_["WGS3"];
+	}
+
+	// Retrieves the Xgemv kernel from the compiled binary
+	auto kernel = Kernel(program_, kernel_name);
+
+	// Sets the kernel arguments
+	kernel.SetArgument(0, static_cast<int>(m_real));
+	kernel.SetArgument(1, static_cast<int>(n_real));
+	kernel.SetArgument(2, GetRealArg(alpha));
+	kernel.SetArgument(3, GetRealArg(beta));
+	kernel.SetArgument(4, static_cast<int>(a_rotated));
+	kernel.SetArgument(5, a_buffer());
+	kernel.SetArgument(6, static_cast<int>(a_offset));
+	kernel.SetArgument(7, static_cast<int>(a_ld));
+	kernel.SetArgument(8, x_buffer());
+	kernel.SetArgument(9, static_cast<int>(x_offset));
+	kernel.SetArgument(10, static_cast<int>(x_inc));
+	kernel.SetArgument(11, y_buffer());
+	kernel.SetArgument(12, static_cast<int>(y_offset));
+	kernel.SetArgument(13, static_cast<int>(y_inc));
+	kernel.SetArgument(14, static_cast<int>(a_conjugate));
+	kernel.SetArgument(15, static_cast<int>(parameter));  // extra parameter used for symm/herm
+	kernel.SetArgument(16, static_cast<int>(kl));         // only used for banded matrices
+	kernel.SetArgument(17, static_cast<int>(ku));         // only used for banded matrices
+
+	// Launches the kernel
+	auto global = std::vector<size_t>{global_size};
+	auto local = std::vector<size_t>{local_size};
+	RunKernel(kernel, queue_, device_, global, local, event_);
+}
+
 void sgemv(tart::command_sequence_ptr sequence,
 	enum CBLAS_ORDER order, enum CBLAS_TRANSPOSE transpose,
 	uint32_t m, uint32_t n, float alpha,
